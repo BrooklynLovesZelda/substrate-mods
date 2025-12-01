37e8272593c59fcdecb806f84b57fb422b6e74d2
TakatoSey <142661838+TakatoSey@users.noreply.github.com>
2025-12-01 20:50:05 +0300

    Add files via upload

---
+++ CatEars/build.bat
@@ -0,0 +1,17 @@
+@echo off
+dotnet build -c Release -v quiet
+if errorlevel 1 (
+    echo.
+    echo Build failed!
+    pause
+    exit /b 1
+)
+
+set "SOURCE=bin\Release\netstandard2.1\CatEarsMod.dll"
+set "DEST=..\BepInEx\plugins\CellHUDMod\CatEarsMod.dll"
+
+if not exist "..\BepInEx\plugins\CellHUDMod\" mkdir "..\BepInEx\plugins\CatEarsMod.dll\"
+copy /Y "%SOURCE%" "%DEST%" >nul
+
+echo Copied to: %DEST%
+pause
