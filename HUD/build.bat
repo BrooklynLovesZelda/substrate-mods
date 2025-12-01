88870626106837bfa258c55732496401b8eec184
TakatoSey <142661838+TakatoSey@users.noreply.github.com>
2025-12-01 20:16:59 +0300

    Create build.bat

---
+++ HUD/build.bat
@@ -0,0 +1,17 @@
+@echo off
+dotnet build -c Release -v quiet
+if errorlevel 1 (
+    echo.
+    echo Build failed!
+    pause
+    exit /b 1
+)
+
+set "SOURCE=bin\Release\netstandard2.1\CellHUDMod.dll"
+set "DEST=..\BepInEx\plugins\CellHUDMod\CellHUDMod.dll"
+
+if not exist "..\BepInEx\plugins\CellHUDMod\" mkdir "..\BepInEx\plugins\CellHUDMod\"
+copy /Y "%SOURCE%" "%DEST%" >nul
+
+echo Copied to: %DEST%
+pause
